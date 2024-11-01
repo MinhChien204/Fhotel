80897c3e8797d364ceba67dc39d13d9260b279a3
truongdka1308 <truongngv1308@gmail.com>
2024-11-01 22:56:03 +0700

    add model room

---
+++ ServerApi/models/room.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const Room = new Schema({
+    name: { type: String, required: true },
+    price: { type: Number, required: true },  // Đổi sang Number nếu `price` là số
+    rating: { type: Number, required: true },
+    description: { type: String, required: true },
+    image: { type: String, required: true },
+    capacity: { type: Number, required: true },
+    status: { type: String, required: true },  // Chuyển sang String để lưu trạng thái như "available"
+    room_code: { type: String, required: true } // Chuyển thành String cho mã phòng dạng "D123"
+}, {
+    timestamps: true
+});
+
+module.exports = mongoose.model('room', Room); // Đặt tên model viết hoa 'Room' để nhất quán với tên file
